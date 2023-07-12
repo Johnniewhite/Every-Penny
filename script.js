4813be626df06d6a16d31ed6484697654278dad1
Adeyinka Inioluwa John <boluwatifeeri@gmail.com>
2023-07-13 00:06:33 +0100

    TEST: test to see it the form adds to db

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
-
+import { getDatabase,ref, set, onValue, } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
   
 
 const firebaseConfig = {
@@ -18,7 +18,8 @@ messagingSenderId: "1042032186383",
 
 appId: "1:1042032186383:web:2cde7e360465e0e63cff7a",
 
-measurementId: "G-93X267ZXWS"
+measurementId: "G-93X267ZXWS",
+databaseURL: "https://everypenny-a7b43-default-rtdb.firebaseio.com/"
 
 };
 
@@ -30,6 +31,7 @@ const analytics = getAnalytics(app);
 
 const auth = getAuth(app);
 
+const database = getDatabase(app);
   
   
 
@@ -62,6 +64,17 @@ const createacctbtn = document.getElementById("create-acct-btn");
 const returnBtn = document.getElementById("return-btn");
 
 const logoutButton = document.getElementById("logoutBtn");
+
+
+
+//   The form Collection
+const titleOfItem = document.getElementById("title");
+const descriptionOfItem = document.getElementById("description")
+const priceOfItem = document.getElementById("price");
+const forWhoItem = document.getElementById("dropdown")
+const addItem = document.getElementById("addButton")
+
+
 
 var email, password, signupEmail, signupPassword, confirmSignupEmail, confirmSignUpPassword;
 
@@ -188,5 +201,40 @@ createacct.style.display = "none";
 
 });
 
+// SAVING THE ENTRY INTO THE DATABASE
 
-
+function saveFormDataToDatabase() {
+    const title = titleOfItem.value;
+    const description = descriptionOfItem.value;
+    const price = priceOfItem.value;
+    const forWho = forWhoItem.value;
+    
+    // Create a new reference to a child node in the database
+    const newItemRef = ref(database, 'items').push();
+    
+    // Set the data for the new item
+    set(newItemRef, {
+      title: title,
+      description: description,
+      price: price,
+      forWho: forWho
+    })
+    .then(() => {
+      // Data saved successfully
+      console.log('Form data saved to database!');
+      
+      // Clear the form inputs after saving
+      titleOfItem.value = '';
+      descriptionOfItem.value = '';
+      priceOfItem.value = '';
+      forWhoItem.value = 'Me';
+    })
+    .catch((error) => {
+      // An error occurred while saving the data
+      console.error('Error saving form data:', error);
+    });
+  }
+  
+  // Add an event listener to the "Add" button
+  addItem.addEventListener('click', saveFormDataToDatabase);
+  

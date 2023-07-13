c2b780e283fa7abfc6db274368d38d95b2fe1def
Adeyinka Inioluwa John <boluwatifeeri@gmail.com>
2023-07-13 01:19:16 +0100

    TEST: I want to test the form from a domain asides localhost

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+console.log("Script Loaded")
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
-import { getDatabase,ref, set, onValue, } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
-  
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
 
 const firebaseConfig = {
 
@@ -26,12 +27,10 @@ databaseURL: "https://everypenny-a7b43-default-rtdb.firebaseio.com/"
   
 
 const app = initializeApp(firebaseConfig);
-
 const analytics = getAnalytics(app);
-
 const auth = getAuth(app);
-
 const database = getDatabase(app);
+
   
   
 
@@ -69,10 +68,10 @@ const logoutButton = document.getElementById("logoutBtn");
 
 //   The form Collection
 const titleOfItem = document.getElementById("title");
-const descriptionOfItem = document.getElementById("description")
+const descriptionOfItem = document.getElementById("description");
 const priceOfItem = document.getElementById("price");
-const forWhoItem = document.getElementById("dropdown")
-const addItem = document.getElementById("addButton")
+const forWhoItem = document.getElementById("dropdown");
+const addButton = document.getElementById("addButton");
 
 
 
@@ -203,40 +202,33 @@ createacct.style.display = "none";
 
 // SAVING THE ENTRY INTO THE DATABASE
 
+addButton.addEventListener("click", saveFormDataToDatabase);
+
 function saveFormDataToDatabase() {
-    const title = titleOfItem.value;
-    const description = descriptionOfItem.value;
-    const price = priceOfItem.value;
-    const forWho = forWhoItem.value;
-    
-    // Create a new reference to a child node in the database
-    const newItemRef = ref(database, 'items').push();
-    
-    // Set the data for the new item
-    set(newItemRef, {
-      title: title,
-      description: description,
-      price: price,
-      forWho: forWho
-    })
+  const title = titleOfItem.value;
+  const description = descriptionOfItem.value;
+  const price = priceOfItem.value;
+  const forWho = forWhoItem.value;
+
+  const newItemRef = ref(database, 'items').push();
+
+  set(newItemRef, {
+    title: title,
+    description: description,
+    price: price,
+    forWho: forWho
+  })
     .then(() => {
-      // Data saved successfully
       console.log('Form data saved to database!');
-      window.alert("Expense Added")
-      
-      // Clear the form inputs after saving
+      window.alert("Expense Added");
+
       titleOfItem.value = '';
       descriptionOfItem.value = '';
       priceOfItem.value = '';
       forWhoItem.value = 'Me';
     })
     .catch((error) => {
-      // An error occurred while saving the data
       console.error('Error saving form data:', error);
-      window.alert("I could not add your expense oo")
+      window.alert("Failed to add expense");
     });
-  }
-  
-  // Add an event listener to the "Add" button
-  addItem.addEventListener('click', saveFormDataToDatabase);
-  
+}
